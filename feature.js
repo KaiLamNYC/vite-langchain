cfba7b52f21248250a9e329a5ca65166bd8117bb
Kai Lam <kailamnyc@gmail.com>
2023-12-01 12:31:13 -0500

    finished translation chain

--- feature.js
+++ feature.js
@@ -5,7 +5,12 @@ import { StringOutputParser } from "langchain/schema/output_parser";
 //MULTIPLE CHAINS
 //https://js.langchain.com/docs/expression_language/cookbook/multiple_chains
 //https://js.langchain.com/docs/expression_language/how_to/map
-import { RunnableSequence } from "langchain/schema/runnable";
+//RUNNABLE PASSTHROUGH
+//https://js.langchain.com/docs/expression_language/how_to/map#manipulating-outputsinputs
+import {
+	RunnablePassthrough,
+	RunnableSequence,
+} from "langchain/schema/runnable";
 
 const openAIApiKey = import.meta.env.VITE_OPENAI_API_KEY;
 const llm = new ChatOpenAI({ openAIApiKey });
@@ -23,18 +28,43 @@ senence with correct grammar:`;
 
 const grammarPrompt = PromptTemplate.fromTemplate(grammarTemplate);
 
+const translationTemplate = `Given a sentence, translate that sentence into {language}
+sentence: {grammatically_correct_sentence}
+translated sentence: `;
+
+const translationPrompt = PromptTemplate.fromTemplate(translationTemplate);
+
 const punctuationChain = RunnableSequence.from([
 	punctuationPrompt,
 	llm,
 	new StringOutputParser(),
 ]);
 
-const chain = RunnableSequence.from([
-	//CHAINING MULTIPLE SEQUENCES
-	{ punctuated_sentence: punctuationChain },
+const grammarChain = RunnableSequence.from([
 	grammarPrompt,
 	llm,
 	new StringOutputParser(),
+]);
+
+const translationChain = RunnableSequence.from([
+	translationPrompt,
+	llm,
+	new StringOutputParser(),
+]);
+
+const chain = RunnableSequence.from([
+	//CHAINING MULTIPLE SEQUENCES
+	{
+		punctuated_sentence: punctuationChain,
+		//TAKING THE ORIGINAL INPUT AND PASSING IT ONTO THE NEXT RUNNABLE
+		original_input: new RunnablePassthrough(),
+	},
+	{
+		grammatically_correct_sentence: grammarChain,
+		//PARSING LANGUAGE FROM THE
+		language: ({ original_input }) => original_input.language,
+	},
+	translationChain,
 ]);
 
 //INVOKING THE RUNNABLE SEQUENCES
